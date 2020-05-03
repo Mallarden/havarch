2a8090d89229f9c4a325d399e410ee5c510db4af
Mallarden <antonanderson.com@gmail.com>
2020-05-04 00:51:46 +0200

    fix

--- template-parts/contact.php
+++ template-parts/contact.php
@@ -5,6 +5,9 @@
 get_header(); ?>
 <div id="primary" class="content">
 <div class="top-container">
+<nav class="nav" role="navigation">
+          <a href="#" onClick="history.back();return false;">← Back</a>
+        </nav>
   <?php
     // TO SHOW THE PAGE CONTENTS
     while ( have_posts() ) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
